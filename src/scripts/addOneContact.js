786f83beb8f605512be8309ce52d492b356e5d4b
Sergey Shulga <shul.sergio@gmail.com>
2024-10-19 16:16:50 +0300

    v 1.1 add data

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,12 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
+const addOneContact = async (number) => {
+  const contacts = await readContacts();
+  console.log('read :', contacts);
+  contacts.push(createFakeContact());
+  console.log('add :', contacts);
+  writeContacts(contacts);
+};
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,13 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
-generateContacts(5);
+const generateContacts = async (number) => {
+  const contactsParse = await readContacts();
+  const newContacts = Array(number).fill(0).map(createFakeContact);
+  const result = [...contactsParse, ...newContacts];
+  console.log(result);
+  writeContacts(result);
+};
+
+generateContacts(3);
